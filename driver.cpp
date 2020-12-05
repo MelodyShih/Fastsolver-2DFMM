6cd9dd7e1b3d2a8ff0d1746eb2a869190adc9da4
TongShanyin <st3255@nyu.edu>
2020-12-05 10:15:15 -0500

    use random qi

--- driver.cpp
+++ driver.cpp
@@ -126,8 +126,8 @@ int main(int argc, char *argv[])
 	// complex<double> array : x, y, q
 	for(int i=0; i<N; i++){
 		x[i] = complex<double>(rand01(), rand01());
-		//q[i] = 10*rand01();
-		q[i] = 1.0;
+		q[i] = 10*rand01();
+		// q[i] = 1.0;
 	}
 	// exact(N, x, q, utrue);
 
